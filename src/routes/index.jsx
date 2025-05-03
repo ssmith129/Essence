76fa606666201538d17af34d51e7733185a0a6a9
ssmith129 <smith.design530@gmail.com>
2025-05-03 16:54:57 -0700

    v8

---
+++ src/routes/index.jsx
@@ -0,0 +1,24 @@
+import { lazy } from 'react';
+import { AuthRoutes } from './auth';
+import { PublicRoutes } from './public';
+import { DashboardRoutes } from './dashboard';
+import { ComponentRoutes } from './components'; // GLOBAL ERROR PAGE
+
+const ErrorPage = lazy(() => import('@/pages/404')); // LANDING / INITIAL PAGE
+
+const Landing = lazy(() => import('@/pages/landing'));
+export const routes = () => {
+  return [// INITIAL / INDEX PAGE
+  {
+    path: '/',
+    element: <Landing />
+  }, // GLOBAL ERROR PAGE
+  {
+    path: '*',
+    element: <ErrorPage />
+  }, // AUTHENTICATION PAGES ROUTES & DIFFERENT AUTH DEMO PAGES ROUTES
+  ...AuthRoutes, // COMPONENTS PAGES ROUTES
+  ...ComponentRoutes, // INSIDE DASHBOARD PAGES ROUTES
+  ...DashboardRoutes, // PAGES ROUTES
+  ...PublicRoutes];
+};
